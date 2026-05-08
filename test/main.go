2ff9a05842e9fc51f0df5715610bd38f302955c7
ahmedsat <ahmedsat303@gmail.com>
2026-05-08 17:16:06 +0300

    Update progress test harness

--- test/main.go
+++ test/main.go
@@ -1,65 +1,54 @@
 package main
 
 import (
-	_ "embed"
 	"fmt"
-	"os"
+	"math/rand"
+	"sync"
+	"time"
 
-	"github.com/ahmedsat/ebda-cli/config"
-	"github.com/ahmedsat/ebda-cli/frappe"
-	"github.com/ahmedsat/ebda-cli/frappe/types"
-	"github.com/ahmedsat/ebda-cli/utils"
+	"github.com/vbauerster/mpb/v8"
+	"github.com/vbauerster/mpb/v8/decor"
 )
 
-func init() {
-	err := config.Configure()
-	if err != nil {
-		panic(err)
-	}
-}
-
-func handelError(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-var f = "auto-sheet"
-
 func main() {
-	switch f {
-	case "auto-sheet":
-		autoSheet()
-	case "follow-up":
-		followUp()
-	default:
-		panic("unknown command")
+	var wg sync.WaitGroup
+	// passed wg will be accounted at p.Wait() call
+	p := mpb.New(mpb.WithWaitGroup(&wg))
+	total, numBars := 100, 3
+	wg.Add(numBars)
+
+	for i := range numBars {
+		name := fmt.Sprintf("Bar#%d:", i)
+		bar := p.AddBar(int64(total),
+			mpb.PrependDecorators(
+				// simple name decorator
+				decor.Name(name),
+				// decor.DSyncWidth bit enables column width synchronization
+				decor.Percentage(decor.WCSyncSpace),
+			),
+			mpb.AppendDecorators(
+				// replace ETA decorator with "done" message, OnComplete event
+				decor.OnComplete(
+					// ETA decorator with ewma age of 30
+					decor.EwmaETA(decor.ET_STYLE_GO, 30, decor.WCSyncWidth), "done",
+				),
+			),
+		)
+		// simulating some work
+		go func() {
+			defer wg.Done()
+			rng := rand.New(rand.NewSource(time.Now().UnixNano()))
+			max := 100 * time.Millisecond
+			for range total {
+				// start variable is solely for EWMA calculation
+				// EWMA's unit of measure is an iteration's duration
+				start := time.Now()
+				time.Sleep(time.Duration(rng.Intn(10)+1) * max / 10)
+				// we need to call EwmaIncrement to fulfill ewma decorator's contract
+				bar.EwmaIncrement(time.Since(start))
+			}
+		}()
 	}
-}
-
-func autoSheet() {
-	// // 11tXfIz9o_cgD-czMTQRRLF9JEkmvNTF5QSmdY6lVQFs
-	// err := sheets.Append(
-	// 	context.Background(),
-	// 	"11tXfIz9o_cgD-czMTQRRLF9JEkmvNTF5QSmdY6lVQFs",
-	// 	"sheet1!A1",
-	// 	[][]any{
-	// 		{"hello", "world"},
-	// 	},
-	// )
-	// handelError(err)
-}
-
-func followUp() {
-	maps, err := frappe.Get[types.MapRecord](nil, nil, nil)
-	handelError(err)
-
-	fmt.Println(len(maps))
-	maps = utils.Filter(maps, func(m types.MapRecord) bool { return m.Color == "#181818" })
-	fmt.Println(len(maps))
-
-	bytes, err := types.RecordsToKML(maps)
-
-	err = os.WriteFile("maps.kml", bytes, 0666)
-	handelError(err)
+	// wait for passed wg and for all bars to complete and flush
+	p.Wait()
 }
